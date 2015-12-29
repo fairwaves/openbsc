e3c4ba35cdfae154fdfe1121845235854e47a4ae
Ivan Kluchnikov <kluchnikovi@gmail.com>
2015-12-29 18:59:21 +0300

    vty: Install sms_destination_cmd element as NITB_NODE

--- openbsc/src/libmsc/vty_interface_layer3.c
+++ openbsc/src/libmsc/vty_interface_layer3.c
@@ -1118,6 +1118,7 @@ int bsc_vty_init_extra(void)
 	install_element(NITB_NODE, &cfg_nitb_subscr_create_only_local_cmd);
 	install_element(NITB_NODE, &cfg_nitb_no_subscr_create_only_local_cmd);
 	install_element(NITB_NODE, &sup_ussd_destination_cmd);
+	install_element(NITB_NODE, &sms_destination_cmd);
 
 	return 0;
 }
