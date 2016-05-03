6bd29aff47329024484a08511ddc55f9c44eb0c3
Sergey Kostanbaev <sergey.kostanbaev@gmail.com>
2016-05-04 01:22:46 +0300

    fixup for SUP first byte header

--- openbsc/include/openbsc/gprs_gsup_messages.h
+++ openbsc/include/openbsc/gprs_gsup_messages.h
@@ -52,9 +52,9 @@ enum gprs_gsup_iei {
 };
 
 enum gprs_gsup_message_type {
-	GPRS_GSUP_MSGT_USSD_MAP			= 0b01111111,
-	GPRS_GSUP_MSGT_MAP                      = 0b01111110,
-	GPRS_GSUP_MSGT_SMS                      = 0b01111101,
+	GPRS_GSUP_MSGT_MAP                      = 0b01111111,
+	GPRS_GSUP_MSGT_SMS                      = 0b01111110,
+	GPRS_GSUP_MSGT_USSD_MAP			= 0b01111101,
 
 	GPRS_GSUP_MSGT_UPDATE_LOCATION_REQUEST	= 0b00000100,
 	GPRS_GSUP_MSGT_UPDATE_LOCATION_ERROR	= 0b00000101,
